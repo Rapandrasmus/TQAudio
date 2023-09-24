d2a2b7261b7444bc4c9346cfdd5afe07de0cb9eb
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-09-24 18:33:16 +0200

    module created + miniaudio implementation and simple playsound function

---
+++ modules/tqaudio/tqaudio.cpp
@@ -0,0 +1,56 @@
+#include "tqaudio.h"
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+{
+    // In playback mode copy data to pOutput. In capture mode read data from pInput. In full-duplex mode, both
+    // pOutput and pInput will be valid and you can move data from pInput into pOutput. Never process more than
+    // frameCount frames.
+}
+
+void TQAudio::play_sound(String p_file) 
+{
+    ma_engine_play_sound(engine, p_file.utf8().get_data(), NULL);
+}
+
+void TQAudio::_bind_methods()
+{
+    ClassDB::bind_method(D_METHOD("play_sound", "file"), &TQAudio::play_sound);
+}
+
+//Just testing miniaudio rn, make this shit better when i figure out what the hell i am doing
+TQAudio::TQAudio() 
+{
+    device = new ma_device;
+    engine = new ma_engine;
+
+    ma_device_config config = ma_device_config_init(ma_device_type_playback);
+    config.playback.format   = ma_format_f32;   
+    config.playback.channels = 2;               
+    config.sampleRate        = 48000;           
+    config.dataCallback      = data_callback;
+
+    if (ma_device_init(NULL, &config, device) != MA_SUCCESS) 
+    {
+        printf("Device initialization failed.");
+        return;
+    }
+
+    if (ma_engine_init(NULL, engine) != MA_SUCCESS) 
+    {
+        printf("Engine initialization failed.");
+        return;
+    }
+
+    ma_device_start(device);
+}
+
+TQAudio::~TQAudio() 
+{
+    ma_device_uninit(device);
+    ma_engine_uninit(engine);
+    delete device;
+    delete engine;
+}
